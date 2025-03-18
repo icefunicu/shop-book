a9b1b375c16b9c1ec3d93ce5e7e022b97563d0e1
netxx <2041487752@gmail.com>
2025-03-19 00:36:09 +0800

    资讯后端代码

---
+++ shop-store-server-master/routes/campus.js
@@ -0,0 +1,13 @@
+//资讯相关
+const express = require('express');
+const router = express.Router();
+const campusController = require('../controllers/campusController');
+
+// 资讯的增删改查
+router.post('/addCampus', campusController.addCampus);
+router.post('/editCampus', campusController.editCampus);
+router.post('/deleteCampus', campusController.deleteCampus);
+router.post('/getCampusList', campusController.getCampusList);
+router.post('/getCampusOne', campusController.getCampusOne);
+
+module.exports = router;
